98b1cf5e1a4648bdf5600644055a0228093a72a6
Madalena Makiesse <62503320+madalenamakiesse@users.noreply.github.com>
2024-04-20 22:33:53 +0100

    Create evaluate_models.sql

---
+++ evaluate_models.sql
@@ -0,0 +1,34 @@
+-- Evaluating the models
+SELECT
+  roc_auc,
+  CASE
+    WHEN roc_auc > .9 THEN 'Good'
+    WHEN roc_auc > .8 THEN 'Fair'    
+    WHEN roc_auc > .7 THEN 'Decent'    
+    WHEN roc_auc > .6 THEN 'Not Good'
+    ELSE 'Poor'
+  END AS Model_Quality
+FROM
+  ML.EVALUATE(MODEL loan.model, (
+    SELECT 
+      *
+    FROM 
+      `loan.train_clean`
+  ));
+
+SELECT
+  roc_auc,
+  CASE
+    WHEN roc_auc > .9 THEN 'Good'
+    WHEN roc_auc > .8 THEN 'Fair'    
+    WHEN roc_auc > .7 THEN 'Decent'    
+    WHEN roc_auc > .6 THEN 'Not Good'
+    ELSE 'Poor'
+  END AS Model_Quality
+FROM
+  ML.EVALUATE(MODEL loan.model1, (
+    SELECT 
+      *
+    FROM 
+      `loan.train_clean`
+  ));
